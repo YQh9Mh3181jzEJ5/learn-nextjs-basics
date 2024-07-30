71b9b486382c8db3efb1d45b00dede2afd57cf16
YQh9Mh3181jzEJ5 <mfghz487k44o6u6ekgub@gmail.com>
2024-07-30 16:15:04 +0900

    feat: add not found

---
+++ app/users/[userId]/not-found.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+export default function UserNotFoundPage() {
+  return (
+    <>
+      <h1 className="text-lg border-b pb-1 mb-1">Error!</h1>
+      <p>User not found!</p>
+      <p className="mt-4">
+        <Link href="/" className="text-blue-500 hove:text-blue-7000">
+          Go Back
+        </Link>
+      </p>
+    </>
+  );
+}
