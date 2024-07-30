b79eb78087a1c63f25046b52e0f41133d6f260e3
YQh9Mh3181jzEJ5 <mfghz487k44o6u6ekgub@gmail.com>
2024-07-30 15:51:20 +0900

    feat: create users

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,21 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <h1>Users</h1>;
+  return (
+    <>
+      <h1 className="text-lg border-b pb-1 mb-1">Users</h1>
+      <ul className="list-disc ml-4">
+        <li>
+          <Link href="/users/0" className="text-blue-500 hove:text-blue-7000">
+            User0
+          </Link>
+        </li>
+        <li>
+          <Link href="/users/1" className="text-blue-500 hove:text-blue-7000">
+            User1
+          </Link>
+        </li>
+      </ul>
+    </>
+  );
 }
---
+++ app/users/[userId]/page.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+interface Props {
+  params: { userId: string };
+}
+
+export default function UserPage(props: Props) {
+  return (
+    <>
+      <h1 className="text-lg border-b pb-1 mb-1">
+        UserPage {props.params.userId}
+      </h1>
+      <p>
+        <Link href="/" className="text-blue-500 hove:text-blue-7000">
+          Go Back
+        </Link>
+      </p>
+    </>
+  );
+}
